ce925eb33b1e7866bc86c6344f1fa691d5ed348e
Tanjim Motin <tanjimotin21@gamil.com>
2018-12-06 20:10:38 -0500

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+function myFunction() {
+	demo.innerHTML ="Click me to change my text color"
+    document.getElementById("demo").style.color = "red";
+}
+
+var box = document.getElementsByClassName("box")[0];
+
+box.addEventListener('click', changeColor)
+
+
+function changeColor(){
+	
+	box.style.backgroundColor ="pink";
+	
+}
+
+
+var box2 = document.getElementById("box2")
+
+box2.addEventListener('click',changeRadius)
+function changeRadius(){
+    box2.style.borderRadius = "50%"
+
+
+}
+
+var box3 = document.getElementById("box3")
+box3.addEventListener('click',changeStyle)
+
+
+function changeStyle(){
+    box3.style.borderRadius = "20%"
+
+
+}
