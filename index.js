e7aec5cca22f89df95d66a9c363b8c3be7ef41f5
Tanjim Motin <tanjimotin21@gmail.com>
2019-01-20 14:54:30 -0500

    My message

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
+<<<<<<< HEAD
 /*function myFunction() {
 	demo.innerHTML ="Click me to change my text color"
     document.getElementById("demo").style.color = "red";
 }
 
 var box = document.getElementsByClassName("box")[0];
+=======
+window.onscroll = function() {myFunction()};
+>>>>>>> 32cc268345c66b8e5e85d38fcae69135034592cf
 
-box.addEventListener('click', changeColor)
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
-
-function changeColor(){
-	
-	box.style.backgroundColor ="pink";
-	
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
 
 
-var box2 = document.getElementById("box2")
+// Scroll back to top feature
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-box2.addEventListener('click',changeRadius)
-function changeRadius(){
-    box2.style.borderRadius = "50%"
-
-
+function scrollFunction() {
+  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+    document.getElementById("navbar").style.opacity= "100";
+    document.getElementById("navbar").classList = "sticky";
+    // document.getElementsByClassName("sticky").style.opacity= "100";
+  } else {
+    document.getElementById("navbar").style.opacity= "0";
+    // document.getElementsByClassName("sticky").style.opacity= "0";
+  }
 }
 
+<<<<<<< HEAD
 var box3 = document.getElementById("box3")
 box3.addEventListener('click',changeStyle)
 
@@ -34,3 +47,10 @@ function changeStyle(){
 
 }
 */
+=======
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+>>>>>>> 32cc268345c66b8e5e85d38fcae69135034592cf
